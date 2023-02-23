a1351b16d7f9df7578a68af5000d689b0514d192
adno88 <adno88@o2.pl>
2023-02-23 21:09:24 +0100

    Created consts to every class

---
+++ main.js
@@ -0,0 +1,7 @@
+const pPoints = document.querySelector('.p-points');
+const cPoints = document.querySelector('c-points');
+const buttons = document.querySelectorAll('.images button');
+const pChoice = document.querySelector('.p-choice');
+const cChoice = document.querySelector('c-choice');
+const result = document.querySelector('.game-result');
+const resetGame = document.querySelector('.reset-game');
