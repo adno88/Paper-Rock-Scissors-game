7f32c190afe60a059f803929a5866593e2b540f4
adno88 <adno88@o2.pl>
2023-02-24 17:27:21 +0100

    Added function which sets points on 0 for player and computer on load web

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const pPoints = document.querySelector('.p-points');
-const cPoints = document.querySelector('c-points');
+const cPoints = document.querySelector('.c-points');
 const buttons = document.querySelectorAll('.images button');
 const pChoice = document.querySelector('.p-choice');
 const cChoice = document.querySelector('c-choice');
@@ -11,3 +11,9 @@ let playerChoice = '';
 let computerPoints = 0;
 let computerChoice = '';
 
+function startGame() {
+    pPoints.innerHTML = playerPoints;
+    cPoints.innerHTML = computerPoints;
+}
+
+window.onload = startGame();
